e921f5f2bcf7ba33d9b776d54b8484aa54a943a4
Nobuo Tsuchiya <develop@m.tsuchi99.net>
2020-01-10 13:58:32 +0900

    change for php7

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Msgpack serializer by only PHP
 
+
+### usage
+
 * __serialize__  
-MsgPack::serialize(mixed $mixed);
+PurePhpMsgPack::serialize(mixed $mixed);
 
 * __unserialize__  
-MsgPack::unserialize(string $binary);
+PurePhpMsgPack::unserialize(string $binary);
+
+
+### ポエム
+
+PHPではobjectのinstanceの取り回しを考えると、やっぱりserializerはmsgpackが好き\
+受け取る側も型を知っていればHidrateできるし
+
+PHP7記法にすればそこそこ速いと聞いて修正してみたけど、
+相変わらずmoduleと比べると話にならない遅さ・・・。
 
--- msgpack.php
+++ msgpack.php
@@ -1,23 +1,25 @@
 <?php
 
-/** 
+/**
  * MsgPack serializer/unserializer (only php)
- * 
- * @category   Msgpack
- * @package    PackageName
- * @author     Nobuo Tsuchiya <develop@m.tsuchi99.net>
- * @version    Release: @package_version@
- * @link       https://github.com/Tsuchiy/purePHPMsgPack
- * @since      Class available since Release 1.0.0
+ *
+ * @category MsgPack
+ * @package  PurePhpMsgPack
+ * @author   Nobuo Tsuchiya <develop@m.tsuchi99.net>
+ * @version  Release: @package_version@
+ * @link     https://github.com/Tsuchiy/PurePhpMsgPack
+ * @since    Class available since Release 1.0.0
  */
-class MsgPack 
+class PurePhpMsgPack
 {
+
     /**
-     * serializer
+     * serialize
+     *
      * @param mixed $value
      * @return string
      */
-    public static function serialize($value)
+    public static function serialize($value): string
     {
         if (is_object($value)) {
             if (method_exists($value, 'serialize')) {
@@ -26,71 +28,72 @@ class MsgPack
                 $value->__wakeup();
             }
         }
-        $binary = self::variable_serializer($value);
-        
-        return $binary;
-    }
-    
-    private static function variable_serializer($value)
-    {
+
         $type = gettype($value);
         switch ($type) {
-            case "NULL" :
-                return self::null_serializer($value);
-            case "boolean" :
-                return self::boolean_serializer($value);
-            case "integer" :
-                return self::integer_serializer($value);
-            case "double" :
-                return self::double_serializer($value);
-            case "string" :
-                return self::string_serializer($value);
-            case "array" :
-                return self::array_serializer($value);
-            case "object" :
-                return self::object_serializer($value);
-            case "resource" :
-            default :
-                trigger_error('[MsgPack] (MsgPack::serialize) type is unsupported, encoded as null',  E_USER_WARNING);
-                return chr(0xc0);
+            case "NULL":
+                return self::getSerializedNull();
+            case "boolean":
+                return self::booleanSerializer($value);
+            case "integer":
+                return self::integerSerializer($value);
+            case "double":
+                return self::doubleSerializer($value);
+            case "string":
+                // phpのstringをバイナリか判定するのは利点がないのでなのでとりあえず全部stringで実装
+                return self::stringSerializer($value);
+            case "array":
+                return self::arraySerializer($value);
+            case "object":
+                return self::objectSerializer($value);
+            case "resource":
+            default:
+                trigger_error(
+                    '[PurePhpMsgPack] (MsgPack::serialize) type is unsupported, encoded as null',
+                    E_USER_WARNING
+                );
+                return self::getSerializedNull();
         }
     }
-    
+
     /**
      * null用serializer
+     *
      * @param null $value
      * @return char
      */
-    private static function null_serializer($value)
+    private static function getSerializedNull(): string
     {
         return chr(0xc0);
     }
-    
+
     /**
      * boolean用serializer
+     *
      * @param boolean $value
      * @return char
      */
-    private static function boolean_serializer($value)
+    private static function booleanSerializer(bool $value): string
     {
-        return $value === true ? chr(0xc3) : chr(0xc2);
+        return $value ? chr(0xc3) : chr(0xc2);
     }
-    
+
     /**
      * integer用serializer
+     *
      * @param integer $value
      * @return string
      */
-    private static function integer_serializer($value)
+    private static function integerSerializer(int $value): string
     {
-        if (($value >= -(1 << 5)) && ($value < (1 << 7))) {
+        if (($value >= - (1 << 5)) && ($value < (1 << 7))) {
             return pack("c", $value);
-        } elseif (($value < 0) && ($value >= -(1 << 7)) ) {
+        } elseif (($value < 0) && ($value >= - (1 << 7))) {
             return chr(0xd0) . pack("c", $value);
-        } elseif (($value < 0) && ($value >= -(1 << 15))) {
+        } elseif (($value < 0) && ($value >= - (1 << 15))) {
             $bin = pack("n", (1 << 16) + $value);
             return chr(0xd1) . chr(ord($bin) | 0x80) . substr($bin, 1);
-        } elseif (($value < 0) && ($value >= -(1 << 31))) {
+        } elseif (($value < 0) && ($value >= - (1 << 31))) {
             $bin = pack("N", (1 << 32) + $value);
             return chr(0xd2) . chr(ord($bin) | 0x80) . substr($bin, 1);
         } elseif (($value > 0) && ($value < (1 << 8))) {
@@ -100,8 +103,11 @@ class MsgPack
         } elseif (($value > 0) && ($value < (1 << 32))) {
             return chr(0xce) . pack("N", $value);
         } elseif (PHP_INT_SIZE == 4) {
-            trigger_error('[MsgPack] (MsgPack::serialize) too large integer, encoded as null',  E_USER_WARNING);
-            return chr(0xc0);
+            trigger_error(
+                '[PurePhpMsgPack] (MsgPack::serialize) too large integer, encoded as null',
+                E_USER_WARNING
+            );
+            return self::getSerializedNull();
         } else {
             $is_negative = false;
             $rtn = chr(0xcf);
@@ -115,33 +121,34 @@ class MsgPack
             $lower = $value % (1 << 32);
             $bin = pack("N", $upper) . pack("N", $lower);
             if ($is_negative) {
-                $bin = chr(ord($bin) | 0x80). substr($bin, 1);
+                $bin = chr(ord($bin) | 0x80) . substr($bin, 1);
             }
             return $rtn . $bin;
         }
     }
-    
+
     /**
      * double用serializer
+     *
      * @param double $value
      * @return string
      */
-    private static function double_serializer($value)
+    private static function doubleSerializer(float $value): string
     {
         // おそらくPHPは単精度は使わないはず
         return chr(0xcb) . strrev(pack("d", $value));
     }
-    
+
     /**
      * string用serializer
      * @param string $value
      * @return string
      */
-    private static function string_serializer($value)
+    private static function stringSerializer(string $value): string
     {
         $len = strlen($value);
-        // peclのmsgpackや本家c++は0xd9に未対応らしい
-        // phpのstringはバイナリなのでバイナリだけど明確で無いのでとりあえずstringで実装
+        // 本家c++は0xd9に未対応らしい
+        // peclのmsgpackにはmsgpack.use_str8_serializationが追加されてる
         if ($len < 32) {
             $bin = '101' . str_pad(decbin($len), 5, "0", STR_PAD_LEFT);
             return chr(bindec($bin)) . $value;
@@ -154,17 +161,20 @@ class MsgPack
             $bin = pack("i", $len);
             return chr(0xdb) . strrev($bin) . $value;
         } else {
-            trigger_error('[MsgPack] (MsgPack::serialize) too long string, encoded as empty',  E_USER_WARNING);
+            trigger_error(
+                '[PurePhpMsgPack] (MsgPack::serialize) too long string, encoded as empty',
+                E_USER_WARNING
+            );
             return chr(bindec('10100000'));
         }
     }
-    
+
     /**
      * map用header作成
      * @param int $cnt // count of items
      * @return string
      */
-    private static function make_map_header($cnt)
+    private static function makeMapHeader(int $cnt): ?string
     {
         if ($cnt < 15) {
             return chr(0x80 | $cnt);
@@ -173,7 +183,7 @@ class MsgPack
         } elseif ($cnt < (1 << 32)) {
             return chr(0xdf) . pack("N", $cnt);
         }
-        trigger_error('[MsgPack] (MsgPack::serialize) too many items to array',  E_USER_ERROR);
+        trigger_error('[PurePhpMsgPack] (MsgPack::serialize) too many items to array', E_USER_ERROR);
         return null;
     }
 
@@ -182,63 +192,63 @@ class MsgPack
      * @param array $value
      * @return string
      */
-    private static function array_serializer($value)
+    private static function arraySerializer(array $value): string
     {
         // phpのarrayは基本ハッシュ
         $rtn = '';
         $cnt = count($value);
-        $rtn .= self::make_map_header($cnt);
+        $rtn .= self::makeMapHeader($cnt);
         foreach ($value as $key => $object) {
             $rtn .= self::serialize($key);
             $rtn .= self::serialize($object);
         }
         return $rtn;
     }
-    
+
     /**
      * object用serializer
      * @param object $value
      * @return string
      */
-    private static function object_serializer($value)
+    private static function objectSerializer(object $value): string
     {
         $rtn = '';
         $refObj = new ReflectionObject($value);
         $properties = $refObj->getProperties();
-        $rtn .= chr(0Xc0);
+        $rtn .= chr(0xc0);
         $className = $refObj->getName();
-        $rtn .= self::string_serializer($className);
+        $rtn .= self::stringSerializer($className);
         $itemCnt = 1;
         foreach ($properties as $property) {
             if ($property->isStatic()) {
                 continue;
             }
             if ($property->isPrivate()) {
-                $rtn .= self::string_serializer(chr(0) . $className . chr(0) . $property->getName());
+                $rtn .= self::stringSerializer(chr(0) . $className . chr(0) . $property->getName());
             } elseif ($property->isProtected()) {
-                $rtn .= self::string_serializer(chr(0) . chr(0x2a) . chr(0) . $property->getName());
+                $rtn .= self::stringSerializer(chr(0) . chr(0x2a) . chr(0) . $property->getName());
             } elseif ($property->isPublic()) {
-                $rtn .= self::string_serializer($property->getName());
+                $rtn .= self::stringSerializer($property->getName());
             }
             $property->setAccessible(true);
             $rtn .= self::serialize($property->getValue($value));
             ++$itemCnt;
         }
-        return self::make_map_header($itemCnt) . $rtn;
+        return self::makeMapHeader($itemCnt) . $rtn;
     }
-    
+
     /**
-     * unserializer
+     * unserialize
      * @param string $binary
      * @return mixed
      */
-    public static function unserialize($binary)
+    public static function unserialize(string $binary)
     {
-        $value = self::variable_unserializer($binary);
+        $value = self::variableUnserializer($binary);
         if (strlen($binary)) {
-            trigger_error('[MsgPack] (MsgPack::unserialize) Extra bytes',  E_USER_WARNING);
+            trigger_error('[PurePhpMsgPack] (MsgPack::unserialize) Extra bytes exist', E_USER_WARNING);
         }
-        
+
         if (is_object($value)) {
             if (method_exists($value, 'unserialize')) {
                 $value->unserialize($binary);
@@ -249,10 +259,16 @@ class MsgPack
         return $value;
     }
 
-    private static function variable_unserializer(&$str)
+    /**
+     * Unserializeの実態
+     *
+     * @param string $str
+     * @return mixed
+     */
+    private static function variableUnserializer(string &$str)
     {
         $ord = ord($str);
-        switch($ord) {
+        switch ($ord) {
             case 0xc0: // null
                 $str = substr($str, 1);
                 return null;
@@ -267,35 +283,35 @@ class MsgPack
                 $str = substr($str, 9);
                 return array_shift($rtn);
             case 0xcc: // uint8
-                $rtn = self::uint_unpack(substr($str, 1, 1));
+                $rtn = self::uintUnpack(substr($str, 1, 1));
                 $str = substr($str, 2);
                 return $rtn;
             case 0xcd: // uint16
-                $rtn = self::uint_unpack(substr($str, 1, 2));
+                $rtn = self::uintUnpack(substr($str, 1, 2));
                 $str = substr($str, 3);
                 return $rtn;
             case 0xce: // uint32
-                $rtn = self::uint_unpack(substr($str, 1, 4));
+                $rtn = self::uintUnpack(substr($str, 1, 4));
                 $str = substr($str, 5);
                 return $rtn;
             case 0xcf: // uint64
-                $rtn = self::uint_unpack(substr($str, 1, 8));
+                $rtn = self::uintUnpack(substr($str, 1, 8));
                 $str = substr($str, 9);
                 return $rtn;
             case 0xd0: // int8
-                $rtn = self::int_unpack(substr($str, 1, 1));
+                $rtn = self::intUnpack(substr($str, 1, 1));
                 $str = substr($str, 2);
                 return $rtn;
             case 0xd1: // int16
-                $rtn = self::int_unpack(substr($str, 1, 2));
+                $rtn = self::intUnpack(substr($str, 1, 2));
                 $str = substr($str, 3);
                 return $rtn;
             case 0xd2: // int32
-                $rtn = self::int_unpack(substr($str, 1, 4));
+                $rtn = self::intUnpack(substr($str, 1, 4));
                 $str = substr($str, 5);
                 return $rtn;
             case 0xd3: // int64
-                $rtn = self::int_unpack(substr($str, 1, 8));
+                $rtn = self::intUnpack(substr($str, 1, 8));
                 $str = substr($str, 9);
                 return $rtn;
             case 0xd9: // string8
@@ -304,23 +320,23 @@ class MsgPack
                 $str = substr($str, $len + 2);
                 return $rtn;
             case 0xda: // string16
-                $len = self::uint_unpack(substr($str, 1, 2));
+                $len = self::uintUnpack(substr($str, 1, 2));
                 $rtn = substr($str, 3, $len);
                 $str = substr($str, $len + 3);
                 return $rtn;
             case 0xdb: // string32
-                $len = self::uint_unpack(substr($str, 1, 4));
+                $len = self::uintUnpack(substr($str, 1, 4));
                 $rtn = substr($str, 5, $len);
                 $str = substr($str, $len + 5);
                 return $rtn;
             case 0xde: // map16,object16
-                $cnt = self::int_unpack(substr($str, 1, 2));
+                $cnt = self::intUnpack(substr($str, 1, 2));
                 $str = substr($str, 3);
-                return self::map_unserializer($str, $cnt);
+                return self::mapUnserializer($str, $cnt);
             case 0xdf: // map32,object32
-                $cnt = self::int_unpack(substr($str, 1, 4));
+                $cnt = self::intUnpack(substr($str, 1, 4));
                 $str = substr($str, 5);
-                return self::map_unserializer($str, $cnt);
+                return self::mapUnserializer($str, $cnt);
             default:
                 if ($ord >= 0xa0 && $ord <= 0xbf) { // fixstring
                     $len = ($ord & 0x1f);
@@ -331,49 +347,73 @@ class MsgPack
                 if ($ord >= 0x80 && $ord <= 0x8f) { // fixmap
                     $cnt = $ord & 0x0f;
                     $str = substr($str, 1);
-                    return self::map_unserializer($str, $cnt);
+                    return self::mapUnserializer($str, $cnt);
                 }
                 if ($ord >= 0x00 && $ord <= 0x7f) { // positive int
                     $str = substr($str, 1);
-                    return (int)$ord;
+                    return (int) $ord;
                 }
                 if ($ord >= 0xe0 && $ord <= 0xff) { // negative int
-                    $rtn = self::uint_unpack(substr($str, 0, 1));
+                    $rtn = self::uintUnpack(substr($str, 0, 1));
                     $str = substr($str, 1);
-                    return (int)$rtn;
+                    return (int) $rtn;
                 }
-                trigger_error('[MsgPack] (MsgPack::unserialize) type is unsupported, encoded as null',  E_USER_ERROR);
+                trigger_error(
+                    '[PurePhpMsgPack] (MsgPack::unserialize) type is unsupported, encoded as null',
+                    E_USER_ERROR
+                );
                 return null;
         }
     }
-    private static function uint_unpack($str)
+
+    /**
+     * uint unserializer
+     *
+     * @param string $str
+     * @return integer
+     */
+    private static function uintUnpack(string $str): int
     {
         $rtn = 0;
-        while($chr = substr($str, 0, 1)) {
+        while ($chr = substr($str, 0, 1)) {
             $rtn = ($rtn << 8) + ord($chr);
             $str = substr($str, 1);
         }
-        return (int)$rtn;
+        return (int) $rtn;
     }
-    private static function int_unpack($str)
+
+    /**
+     * int unserializer
+     *
+     * @param string $str
+     * @return integer
+     */
+    private static function intUnpack(string $str): int
     {
-        $top = substr($str , 0, 1);
+        $top = substr($str, 0, 1);
         if (ord($top) < 0x80) {
-            return self::uint_unpack($str);
+            return self::uintUnpack($str);
         } else {
-            return (int)( (int)(1 << ((8 * strlen($str)) - 1))
-                        - self::uint_unpack(chr(ord($str) & 0x7f) . substr($str ,1)));
+            return (int) ((int) (1 << ((8 * strlen($str)) - 1))
+                - self::uintUnpack(chr(ord($str) & 0x7f) . substr($str, 1)));
         }
     }
 
-    private static function map_unserializer(&$str, $item_count)
+    /**
+     * array/object unserializer
+     *
+     * @param string $str
+     * @param integer $item_count
+     * @return array|object
+     */
+    private static function mapUnserializer(string &$str, int $item_count)
     {
         $array_keys = array();
         $array_values = array();
         $null_key_value = null;
         for ($i = 0; $i < $item_count; ++$i) {
-            $key = self::variable_unserializer($str);
-            $value = self::variable_unserializer($str);
+            $key = self::variableUnserializer($str);
+            $value = self::variableUnserializer($str);
             if (is_null($key)) {
                 $null_key_value = $value; // 多分class name
             } else {
@@ -383,15 +423,23 @@ class MsgPack
         }
         if (!is_null($null_key_value)) {
             if (is_string($null_key_value) && class_exists($null_key_value)) {
-                return self::object_unserializer($null_key_value, $array_keys, $array_values);
+                return self::objectUnserializer($null_key_value, $array_keys, $array_values);
             }
             $array_keys[] = "";
             $array_values[] = $null_key_value;
         }
         return array_combine($array_keys, $array_values);
     }
-    
-    private static function object_unserializer($class_name, array $array_keys, array $array_values)
+
+    /**
+     * object unserializer
+     *
+     * @param string $class_name
+     * @param array $array_keys
+     * @param array $array_values
+     * @return object
+     */
+    private static function objectUnserializer(string $class_name, array $array_keys, array $array_values): object
     {
         @$obj = new $class_name;
         $refObj = new ReflectionObject($obj);
@@ -406,8 +454,11 @@ class MsgPack
                     // is Private
                     $header_str = chr(0) . $class_name . chr(0);
                     if (substr($key, 0, strlen($header_str)) !== ($header_str)) {
-                         trigger_error('[MsgPack] (MsgPack::unserialize) object has wrong property name',  E_USER_ERROR);
-                         continue;
+                        trigger_error(
+                            '[PurePhpMsgPack] (MsgPack::unserialize) object has wrong property name',
+                            E_USER_ERROR
+                        );
+                        continue;
                     }
                     $property_name = substr($key, strlen($header_str));
                 }
